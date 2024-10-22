e2ef98b12d8ed4f737ba337778cf1482f0bbc4a0
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2024-10-22 21:21:34 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Routes , Route } from 'react-router-dom'
+import Authpage from './pages/auth'
+
+function App() {
+  
+
+  return (
+    
+    <Routes>
+      <Route path='/auth' element={<Authpage/>}/>
+    </Routes>
+   
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// CORS middleware
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Database connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("mongodb is connected"))
+  .catch((e) => console.log(e));
+
+//routes configaration
+
+
+
+
+// Global error handler middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+    });
+});
+
+// Start the server
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+        process.exit(1);
+    }
+    console.log(`Server is running on port ${PORT}`);
+});
